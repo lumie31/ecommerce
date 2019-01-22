9f72a71276f5c9f58a30b5aa94d698077d796821
Olumide <olumideokedusi@gmail.com>
2019-01-22 22:07:03 +0100

    Define root styles and import components in app.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <div className="container">
-          <div className="row">
-            <div className="col">First column</div>
-            <div className="col">second column</div>
-          </div>
-        </div>
+        <Navbar />
+        <ProductList />
+        <Details />
+        <Cart />
+        <Default />
       </React.Fragment>
     );
   }
