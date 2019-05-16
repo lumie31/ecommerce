6c81c01c0e6f3b49461c773a2310d4d591502926
Olumide <olumideokedusi@gmail.com>
2019-05-16 03:49:15 +0100

    Pass props to styled component to modify style

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,17 +4,18 @@ export const MyButton = styled.button`
   background: transparent;
   border-radius: 5px;
   border: 1.5px solid palevioletred;
-  color: palevioletred;
+  border-color: ${props => (props.cart ? "var(--mainDark)" : "palevioletred")};
+  color: ${props => (props.cart ? "var(--mainDark)" : "palevioletred")};
   margin: 0 1em;
   padding: 0.25em 1em;
   cursor: pointer;
   text-transform: capitalize;
-  transition: all 0.5s ease-in-out;
+  transition: all 1s ease-in-out;
   &:focus {
     outline: none;
   }
   &:hover {
-    background: palevioletred;
+    background: ${props => (props.cart ? "var(--mainDark)" : "palevioletred")};
     color: white;
   }
 `;
