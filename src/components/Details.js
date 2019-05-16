cdaf5aeba8e90eb08e215eb047900f44429f721c
Olumide <olumideokedusi@gmail.com>
2019-05-16 03:43:51 +0100

    Implement detail view of products and add necessary style

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,11 +1,69 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { ProductConsumer } from "../context";
+import { MyButton } from "./Button";
 
 export default class Details extends Component {
   render() {
     return (
-      <div>
-        <h3>Hello from details</h3>
-      </div>
+      <ProductConsumer>
+        {val => {
+          const {
+            id,
+            title,
+            img,
+            info,
+            inCart,
+            price,
+            company
+          } = val.detailProduct;
+          return (
+            <div className="container py-2">
+              <div className="row">
+                <div className="col-10 mx-auto text-center text-slanted text-blue my-4">
+                  <h1>{title}</h1>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-10 mx-auto col-md-6 my-3">
+                  <img className="img-fluid" src={img} alt="Product" />
+                </div>
+                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                  <h2>Model: {title}</h2>
+                  <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
+                    Made by: <span className="text-uppercase">{company}</span>
+                  </h4>
+                  <h4>
+                    <strong>
+                      Price: <span>£</span>
+                      {price}.00
+                    </strong>
+                  </h4>
+                  <p className="text-capitalize font-weight-bold mt-3 mb-0">
+                    Some info about product:{" "}
+                  </p>
+                  <p className="text-muted text-justify lead">{info}</p>
+                  <div>
+                    <Link to="/">
+                      <MyButton className="m-0">Back to Products</MyButton>
+                    </Link>
+                    <MyButton
+                      className="ml-3"
+                      cart
+                      disabled={inCart ? true : false}
+                      onClick={() => {
+                        val.addToCart(id);
+                      }}
+                    >
+                      {inCart ? "In cart" : "add to cart"}
+                    </MyButton>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        }}
+      </ProductConsumer>
     );
   }
 }
